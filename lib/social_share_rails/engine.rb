b13da59823f94b9eb67dd71ef035464f21816408
Gleydson Tavares <tavares.gleydson@gmail.com>
2025-01-07 17:12:15 -0300

    Enhance asset management by adding paths for JavaScript, stylesheets, and images; update precompilation configuration for assets

--- lib/social_share_rails/engine.rb
+++ lib/social_share_rails/engine.rb
@@ -2,10 +2,18 @@ module SocialShareRails
   class Engine < ::Rails::Engine
     isolate_namespace SocialShareRails
 
+    # Add the gem's assets to the Rails asset pipeline
+    initializer "social_share_rails.assets.paths" do |app|
+      app.config.assets.paths << root.join("app", "assets", "javascripts")
+      app.config.assets.paths << root.join("app", "assets", "stylesheets")
+      app.config.assets.paths << root.join("app", "assets", "images")
+    end
+
+    # Configure the files for precompilation
     initializer "social_share_rails.assets.precompile" do |app|
       app.config.assets.precompile += %w[
-        social_share_rails/social_share.scss
         social_share_rails/social_share.js
+        social_share_rails/social_share.scss
         social_share_rails/*.svg
         social_share_rails/*.png
         social_share_rails/*.jpg
@@ -13,18 +21,11 @@ module SocialShareRails
       ]
     end
 
-    initializer "social_share_rails.assets.paths" do |app|
-      app.config.assets.paths << root.join("app", "assets", "images")
-    end
-
+    # Include the helper in the context of Rails views
     initializer "social_share_rails.helpers" do
       ActiveSupport.on_load(:action_view) do
         include SocialShareRails::SocialShareHelper
       end
-    end
-
-    initializer "social_share_rails.assets.manifest" do |app|
-      app.config.assets.paths << root.join("app", "assets")
     end
   end
 end
